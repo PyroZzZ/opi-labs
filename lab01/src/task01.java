6e7237907ff4de8b7aecfa8f6cff26cf57c01256
user <732603@gmail.com>
2024-03-22 20:23:38 +0400

    lab01

---
+++ lab01/src/task01.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+
+public class task01 {
+    static int N = 10;
+    static int[] arr = new int[N];
+
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        System.out.println("Source array:");
+        //arr = new int[]{1, 2, 0, 4, 5, 6, 7, 8, 9, 0};
+        //arr = new int[]{1,2,3,3,3,3,7,8,9,0};
+        for (int i = 0; i < N; i++) {
+            arr[i] = r.nextInt(10);
+            System.out.printf("%2d", arr[i]);
+        }
+
+        // Поиск повторяющихся
+        Integer tmp = null;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] == arr[j]) {
+                    tmp = arr[i];
+                    deleteEl(tmp);
+                    deleteEl(tmp);
+                    deleteEl(tmp);
+                    if (i > 0) i--;
+                }
+            }
+        }
+
+
+        System.out.println("\nChanged array:");
+        for (int i = 0; i < N; i++) {
+            System.out.printf("%2d", arr[i]);
+        }
+    }
+
+    public static void deleteEl(int value) {
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == value) {
+                if (i + 1 == N) {
+                    N--;
+                    return;
+                }
+
+                for (int j = i; j < N - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                N--;
+                return;
+            }
+        }
+    }
+}
---
+++ lab01/src/task02.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+public class task02 {
+    static int N = 5;
+    static int M = 4;
+    static int[][] arr = new int[N][M];
+
+    public static void main(String[] args) {
+        int tmp = 0;
+        int tmp_col = 0;
+        Random r = new Random();
+
+        System.out.println("Source array:");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = r.nextInt(100) - 50;
+                System.out.printf("%4d", arr[i][j]);
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] < 0 && arr[i][j] <= tmp) {
+                    tmp = arr[i][j];
+                    tmp_col = j;
+                }
+            }
+        }
+
+        deleteColumn(tmp_col);
+
+        System.out.println("\nChanged array:");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.printf("%4d", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void deleteColumn(int col) {
+        if (col == M) {
+            M--;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = col; j < M; j++) {
+                if (j + 1 < M) {
+                    arr[i][j] = arr[i][j + 1];
+                }
+            }
+        }
+        M--;
+    }
+}
